d971e5d6dff3e51dd4ed777d0708afc9019a01f3
safi <safisafikuls@gmail.com>
2021-07-09 00:26:26 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+
+  const addTodo = (e) => {
+    e.preventDefault()
+    setTodos([...todos, input])
+    setInput("")
+  }
+
   return (
     <div className="App">
       <h1>Welcome to my TODO list!</h1>
-      <input type="text" />
-      <button>Add todo</button>
+
+      <form>
+        <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
+        <button type="submit" onClick={addTodo}>Add todo</button>
+      </form>
+
+      <h2>List of Todos</h2>
+      {todos.map((todo) => (
+        <p>{todo}</p>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
